6226b81cd693f3421bd17d9763e391c2343b75a5
John.nie <John-lee@2001594.no-reply.drupal.org>
2019-05-20 17:53:57 +0800

    Fix.

--- drupal/build.sh
+++ drupal/build.sh
@@ -2,22 +2,22 @@
 
 docker-compose up -d
 
-docker rm -f drupal_alpine
-docker run -d --name drupal_alpine neibrs/drupal_alpine
-if [ -d ./web.old ]; then
-  sudo rm -rf ./web.old
+docker rm -f drupal
+docker run -d --name drupal neibrs/drocker:drupal
+if [ -d ../web.old ]; then
+  sudo rm -rf ../web.old
 fi
-if [ -d ./web ]; then
-  mv ./web ./web.old
+if [ -d ../web ]; then
+  mv ../web ../web.old
 fi
-sudo docker cp drupal_alpine:/var/www/html ./web
-docker stop drupal_alpine
-docker rm drupal_alpine
+sudo docker cp drupal:/var/www/html ../web
+docker stop drupal
+docker rm drupal
 
 # For linux
-#sudo chown -R apache.apache ./web
-#sudo chmod -R g+w ./web
+#sudo chown -R apache.apache ../web
+#sudo chmod -R g+w ../web
 
 # For Mac
-sudo chown -R $USER ./web
-sudo chmod -R g+w ./web
+sudo chown -R $USER ../web
+sudo chmod -R g+w ../web
