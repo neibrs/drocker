da820984957ee3117e35797d9b8c05084b77d5d1
John.nie <John-lee@2001594.no-reply.drupal.org>
2019-05-20 15:34:22 +0800

    Fix docker files.

---
+++ drupal/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker pull neibrs/drupal_alpine
+
+docker rm -f drupal_alpine
+docker run -d --name drupal_alpine neibrs/drupal_alpine
+if [ -d ./web.old ]; then
+  sudo rm -rf ./web.old
+fi
+if [ -d ./web ]; then
+  mv ./web ./web.old
+fi
+sudo docker cp drupal_alpine:/var/www/html ./web
+docker stop drupal_alpine
+docker rm drupal_alpine
+
+sudo chown -R apache.apache ./web
+sudo chmod -R g+w ./web
+
